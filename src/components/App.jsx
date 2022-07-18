40be97606309d422f14f19e0be217bc86444d356
Dmitry Davidyk <91990771+DmitryDavidyuk@users.noreply.github.com>
2022-07-19 01:41:20 +0300

    alert

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,7 +23,25 @@ class App extends Component {
       number,
     };
 
-    this.setState(({ contacts }) => ({ contacts: [contact, ...contacts] }));
+    const { contacts } = this.state;
+
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      alert(`${name} is already in contacts.`);
+    } else if (contacts.find(contact => contact.number === number)) {
+      alert(`${number} is already in contacts.`);
+    } else if (name.trim() === '' || number.trim() === '') {
+      alert("Enter the contact's name and number phone!");
+    } else if (!/\d{3}[-]\d{2}[-]\d{2}/g.test(number)) {
+      alert('Enter the correct number phone!');
+    } else {
+      this.setState(({ contacts }) => ({
+        contacts: [contact, ...contacts],
+      }));
+    }
   };
 
   deleteContact = id => {
